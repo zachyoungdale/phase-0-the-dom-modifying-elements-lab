f15d22d05cf9cfe121aa143109bc843b6ca1f385
Zachary Youngdale <zyoungdale@gmail.com>
2022-09-02 15:34:13 -0400

    commit tree 681e69fb600f41618ee6ec012f68fce04760d31d

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+const main = document.getElementById('main');
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+newHeader.innerHTML = 'Zach is the champion';
+
+
+
